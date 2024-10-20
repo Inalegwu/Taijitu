9f350bb253b2d3a8861080712bc3524a307d3750
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-10-20 23:52:32 +0000

    placeholder for health checks

--- src/health.ts
+++ src/health.ts
@@ -1,22 +1,52 @@
-import { Context, Duration, Effect, Layer, Schedule } from "effect";
+import { Context, Duration, Effect, Layer, Schedule, Data } from "effect";
 import * as Servers from "./state";
+import { HttpClient } from "@effect/platform";
+
+class HealthCheckError extends Data.TaggedError("health-check-error")<{
+	cause: unknown;
+}> {}
 
 const make = Effect.gen(function* () {
 	yield* Effect.logInfo("Starting Health Check Service");
 
-	const servers = yield* Servers.make;
+	const serverState = yield* Servers.make;
+	const servers = yield* serverState.get;
+
 	const health = Effect.repeat(
 		Effect.gen(function* () {
-			yield* Effect.logInfo("health check");
+			const address = servers.map((server) => server.address);
+
+			yield* Effect.forEach(
+				address,
+				(address) =>
+					Effect.gen(function* () {
+						const httpClient = yield* HttpClient.HttpClient;
+
+						yield* Effect.try({
+							try: () => httpClient.get(`${address}/health`),
+							catch: (error) => new HealthCheckError({ cause: error }),
+						});
+					}),
+				{
+					concurrency: "unbounded",
+				},
+			);
 		}),
 		Schedule.exponential(Duration.millis(500)),
 	);
 
 	yield* Effect.acquireRelease(
-		Effect.andThen(Effect.logInfo("HealthCheck Service Started"), health),
+		Effect.andThen(
+			Effect.logInfo("HealthCheck Service Started"),
+			Effect.forkDaemon(health),
+		),
 		() => Effect.logInfo("Stopped Health Check Service"),
 	);
-});
+}).pipe(
+	Effect.annotateLogs({
+		module: "health-check",
+	}),
+);
 
 export class Health extends Context.Tag("healthcheck-service")<Health, void>() {
 	static Live = Layer.effect(this, make);
--- src/state.ts
+++ src/state.ts
@@ -5,15 +5,19 @@ type ServerState = Array<
 	Readonly<{
 		address: string;
 		inUse: boolean;
+		healthy: boolean;
 	}>
 >;
 
 export class Servers {
 	update: (address: string, inUse: boolean) => Effect.Effect<void>;
+	get: Effect.Effect<ServerState>;
 
 	constructor(private value: Ref.Ref<ServerState>) {
-		this.update = (addr: string, inUse: boolean) =>
+		this.update = (addr: string, inUse?: boolean, healthy?: boolean) =>
 			Ref.update(this.value, (a) => a);
+
+		this.get = Ref.get(this.value);
 	}
 }
 
@@ -25,6 +29,7 @@ export const make = Effect.andThen(
 			conf.servers.map((s) => ({
 				address: s,
 				inUse: false,
+				healthy: true,
 			})),
 		);
 	}),

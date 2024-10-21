d6595dc192473608e6491b5156c0e8e7296a1e20
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-10-21 01:07:16 +0000

    attempting to work on interceptor

--- src/interceptor.ts
+++ src/interceptor.ts
@@ -1,5 +1,6 @@
 import { Context, Effect, Layer } from "effect";
 import * as Servers from "./state";
+import { HttpRouter, HttpServer } from "@effect/platform";
 
 const make = Effect.gen(function* () {
 	const serverState = yield* Servers.make;

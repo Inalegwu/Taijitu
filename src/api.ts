b2e8f204a58ec7a4fd5860ccd1be74426f4a95ee
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-10-21 00:01:48 +0000

    working on interception listener

--- src/api.ts
+++ src/api.ts
@@ -1,1 +1,15 @@
+import { HttpRouter, HttpServer } from "@effect/platform";
+import { Effect } from "effect";
 
+const router = HttpRouter.empty.pipe(
+	HttpRouter.get(
+		"*",
+		Effect.sync(() => {
+			throw new Error("TODO");
+		}),
+	),
+);
+
+const app = router.pipe(HttpServer.serve);
+
+export default app;
